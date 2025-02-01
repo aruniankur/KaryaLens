fcc959038dab3dc74286755b4bf3ac25c7d5d6b3
aruniankur <arunitowardnew@gmail.com>
2025-02-01 15:06:59 +0530

    added html pages

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html', title="Welcome", message="Hello, Flask with Jinja!")
 
 @app.route('/hello2', methods=['GET'])
 def hello_world2():
